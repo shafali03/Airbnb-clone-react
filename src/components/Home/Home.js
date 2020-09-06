8e1dba4c4028a1e4144a4c657e37a4e2f0c768c3
shafali03 <shaf03sm@gmail.com>
2020-09-06 19:08:15 +0100

    Created a banner which displays show date button with show and hide functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Home.css'
+import Banner from '../Banner/Banner'
 
 function Home() {
   return (
     <div className="home">
-      <h1>Home components</h1>
+      <Banner />
     </div>
   )
 }
