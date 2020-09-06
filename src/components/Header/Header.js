a1936885f51ed9450ca678767b2c787ac91e9bc3
shafali03 <shaf03sm@gmail.com>
2020-09-06 14:04:55 +0100

    Added airbnb logo and created home component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__icon"
+        src="https://res.cloudinary.com/shafali/image/upload/v1599397277/airbnblogo-removebg-preview_bph0ms.png" alt="" />
+    </div>
+  )
+}
+
+export default Header
