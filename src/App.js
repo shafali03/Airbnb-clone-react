2701782b107d67a784d41eb14db3513242171959
shafali03 <shaf03sm@gmail.com>
2020-09-07 14:46:13 +0100

    Created a simple footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home/Home'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
