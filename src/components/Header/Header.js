1e104e4a12279a1e38b9f54d803d357dd0e00521
shafali03 <shaf03sm@gmail.com>
2020-09-08 11:12:31 +0100

    Create a link to navigate back to home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,16 @@ import SearchIcon from '@material-ui/icons/Search';
 import { Avatar } from '@material-ui/core'
 import LanguageIcon from '@material-ui/icons/Language';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__icon"
-        src="https://res.cloudinary.com/shafali/image/upload/v1599397277/airbnblogo-removebg-preview_bph0ms.png" alt="" />
+      <Link to='/'>
+        <img
+          className="header__icon"
+          src="https://res.cloudinary.com/shafali/image/upload/v1599397277/airbnblogo-removebg-preview_bph0ms.png" alt="" />
+      </Link>
 
       <div className="header__center">
         <input type="text" />
